3e07de40b3ae5882f68b28d2e7f9a4cab259720f
Giancarlo Peris <gperis93@gmail.com>
2021-02-20 15:02:45 +0000

    Change SQLite library

--- go.mod
+++ go.mod
@@ -4,8 +4,9 @@ go 1.15
 
 require (
 	github.com/bwmarrin/discordgo v0.23.2
-	github.com/mattn/go-sqlite3 v1.14.6
+	github.com/mattn/go-sqlite3 v1.14.6 // indirect
 	github.com/spf13/viper v1.7.1
 	gopkg.in/yaml.v2 v2.4.0 // indirect
+	modernc.org/sqlite v1.8.7
 	mvdan.cc/xurls/v2 v2.2.0
 )
--- pkg/database/main.go
+++ pkg/database/main.go
@@ -3,8 +3,8 @@ package database
 import (
 	"database/sql"
 	"github.com/gperis/forza-bot/pkg/config"
-	_ "github.com/mattn/go-sqlite3"
 	"log"
+	_ "modernc.org/sqlite"
 )
 
 type conf struct {
@@ -18,7 +18,7 @@ func init() {
 }
 
 func OpenDatabase() (db *sql.DB) {
-	db, err := sql.Open("sqlite3", moduleConf.DbPath)
+	db, err := sql.Open("sqlite", moduleConf.DbPath)
 	if err != nil {
 		log.Fatal(err)
 	}
